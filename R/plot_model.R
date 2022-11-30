e5579f3b44efc5591d2853c47b2e3267b40db5b9
Jo Inge Arnes <jo.i.arnes@uit.no>
2022-11-30 22:31:58 +0100

    some adjustments, with embrace operator and other things

--- R/plot_model.R
+++ R/plot_model.R
@@ -6,11 +6,7 @@
 #' @export
 #' @examples
 #' plot_model(d, y, x, my_mod)
-
 plot_model <- function(dataset, dependent, independent, model) {
-  dependent <- rlang::enquo(dependent)
-  independent <- rlang::enquo(independent)
-
   # The confidence bands has to be in the scale of the link function,
   # and we need to get it's inverse for plotting
   ilink <- stats::family(model)$linkinv
@@ -25,10 +21,54 @@ plot_model <- function(dataset, dependent, independent, model) {
       upr = ilink(fit_link + (2 * se_link)),
       lwr = ilink(fit_link - (2 * se_link)))
 
-  ggplot2::ggplot(d, ggplot2::aes(x = !!independent, y = !!dependent)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_ribbon(data = d,
-      ggplot2::aes(ymin = lwr, ymax = upr), alpha = 0.2) +
-    ggplot2::geom_line(ggplot2::aes(y = pred),
-      color = "blue", linewidth = 0.75)
+  ggplot2::ggplot(d,
+      ggplot2::aes(x = {{ independent }}, y = {{ dependent }})) +
+  ggplot2::geom_point() +
+  ggplot2::geom_ribbon(data = d,
+    ggplot2::aes(ymin = lwr, ymax = upr), alpha = 0.2) +
+  ggplot2::geom_line(ggplot2::aes(y = pred),
+    color = "blue", size = 1)
 }
+
+#region code for debugging
+# main <- function() {
+#   library(dplyr)
+
+#   # d <- read.table(
+#   #   "~/datasets/human_penguin/explorepenguin_share_complete_cases.csv",
+#   #   sep = ",", header = TRUE)
+#   # d <- d %>%
+#   #     drop_na(nwsize) %>%
+#   #     drop_na(age) %>%
+#   #     mutate(age_years = 2022 - age, age_dec = age_years / 10)
+
+
+#   # # Just to make is the same as the fields in the synthetic data
+#   # d$Independent <- d$age_dec
+#   # d$Dependent <- d$nwsize
+#   # d$SignalMeasured <- d$Dependent
+
+#   # # Shuffle the rows
+#   # set.seed(7)
+#   # d <- d[sample(1:nrow(d)), ]
+
+#   # # Bootstrap the data to create a training and a test set
+#   # n_split <- trunc(nrow(d) * 0.5)
+#   # d_full <- d
+#   # d <- d_full[1:n_split, ]
+#   # d_test <- d_full[(n_split + 1):nrow(d_full), ]
+
+#   # Synthetic data sets
+
+#   file_name <- "../paper-3-package/regressionspaper/synthetic_linear.csv"
+#   file_name_test <- "../paper-3-package/regressionspaper/synthetic_linear_test.csv"
+
+#   d <- read.table(file_name, sep = ",", header = TRUE)
+#   d_test <- read.table(file_name_test, sep = ",", header = TRUE)
+
+#   mod <- cladina::model_by_count(d, Dependent, Independent, 4)
+#   cladina::plot_model(d, Dependent, Independent, mod)
+
+#   print("ok")
+# }
+#endregion
